394aeec46097131e6f89cc43980e70e65c712545
Jack Delahunt <20085409@mail.wit.ie>
2021-12-27 21:13:49 +0000

    Feat: added tilemap to level 2

--- game/Level2.h
+++ game/Level2.h
@@ -6,11 +6,18 @@ public:
     void attach(WillieNelson::Game &game) override {
         auto texture = WillieNelson::Resources::Current()->load_texture("../resources/battle-location-top-down-game-tileset-pack/PNG/Blocks/Block_A_01.png");
 
+        auto tilemap_entity = WillieNelson::Entity::New();
+        auto tilemap_component = tilemap_entity->add_component<WillieNelson::TilemapComponent>();
+        tilemap_component->load_from_disk("level_2.json");
+        tilemap_component->rebuild();
+        tilemap_entity->transform.scale = sf::Vector2f(0.2f, 0.2f);
+
         auto game_object_1 = WillieNelson::Entity::New();
         auto sprite_1 = game_object_1->add_component<WillieNelson::SpriteComponent>();
         game_object_1->add_component<PlayerController>();
         sprite_1->set_texture(texture);
 
+        game.add_entity(tilemap_entity);
         game.add_entity(game_object_1);
     }
 };

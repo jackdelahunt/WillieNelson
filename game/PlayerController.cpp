97d13c33e776920635f5ab8165c5255c7d020e42
MrSpeedy68 <mrspeedygamer123@gmail.com>
2021-12-30 17:17:34 +0000

    added 17 gun types

--- game/PlayerController.cpp
+++ game/PlayerController.cpp
@@ -67,6 +67,7 @@ void PlayerController::movement(std::vector<sf::Event> &events) {
 
 void PlayerController::shooting(std::vector<sf::Event> &events) {
     for (auto& event : events) {
+
         if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left && m_ammo > 0) {
             auto mouse_pos = sf::Mouse::getPosition(WillieNelson::Game::Active()->window());
             auto delta_vector = sf::Vector2f((float)mouse_pos.x - entity->transform.position.x, (float)mouse_pos.y - entity->transform.position.y);
@@ -76,7 +77,6 @@ void PlayerController::shooting(std::vector<sf::Event> &events) {
             if(m_shooting_sound) m_shooting_sound->play();
 
             create_bullet(normal_vector);
-
 
             if (m_ammo_text != nullptr) {
                 m_ammo_text->set_text("Ammo : " + std::to_string(--m_ammo));
